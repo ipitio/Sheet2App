268338443ca434e23b288fc8c82d098587d32364
allen-siu <allenlsiu@gmail.com>
2023-03-14 19:56:00 -0400

    Created models for db schema and created CreateCreator api call

--- server/s2a_api/views.py
+++ server/s2a_api/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import mysql_db.queries as queries
 
 # Test endpoint
 def test_endpoint(request):
     return HttpResponse("Test endpoint reached")
+
+
+def create_creator(request):
+    queries.create_creator("test@gmail.com")
+    return HttpResponse("create creator")
--- server/server/settings.py
+++ server/server/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import mysql_db.auth as auth
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': auth.DATABASE,
+        'USER': auth.USER,
+        'PASSWORD': auth.PASSWORD,
+        'HOST': auth.HOST,
+        'PORT': auth.PORT
     }
 }
 
