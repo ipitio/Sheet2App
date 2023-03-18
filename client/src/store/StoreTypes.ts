b1c5558039f836aa21b66ee3a8583dee6697d0be
alexanderleong1 <alexander.leong@stonybrook.edu>
2023-03-18 00:15:38 -0400

    added store types for globalstore

--- client/src/store/StoreTypes.ts
+++ client/src/store/StoreTypes.ts
@@ -9,3 +9,19 @@ export interface DataSource {
     spreadsheetID: number;
     spreadsheetIdx: number;
 }
+
+export interface Role {
+    roleName: string,
+    accessibleViews: View[]
+}
+
+export interface View {
+    name: string,
+    spreadsheetURL: string
+}
+
+export enum Modal {
+    AddRecordModal,
+    EditRecordModal,
+    DeleteRecordModal
+}
