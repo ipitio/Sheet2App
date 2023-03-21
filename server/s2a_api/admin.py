2267ba5fe4cd3ef01f81a17c4b8a81b06208628d
ipitio <21136719+ipitio@users.noreply.github.com>
2023-03-21 14:44:03 -0400

    model tests

--- server/s2a_api/admin.py
+++ server/s2a_api/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+admin.site.register(GlobalDevelopers)
 admin.site.register(Creator)
 admin.site.register(Application)
 admin.site.register(Datasource)
 admin.site.register(DatasourceColumn)
-admin.site.register(AppData)
 admin.site.register(TableView)
 admin.site.register(DetailView)
+admin.site.register(TableViewPerm)
+admin.site.register(DetailViewPerm)
+admin.site.register(TableViewViewableColumn)
+admin.site.register(DetailViewEditableColumn)
