9311c94f6fb41ff6ae12e353f4cd7ebb38628cb9
alexanderleong1 <alexander.leong@stonybrook.edu>
2023-03-21 00:08:25 -0400

    test cases for sheets api

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from server.api.sheets.sheets_api import *
+
 # Create your tests here.
+class SheetsAPITestCase(TestCase):
+    spreadsheet_id='1GxYsHB5FOpZRRDDk0e5E5MpuHKpj_U7C7v6ubhcwNpQ'
+    sheet_id=0
+
+    def test_get_data_when_sheet_id_is_none(self):
+        TestCase.assertEqual(get_data(self.spreadsheet_id), ['First', 'Read', 'Data'])
