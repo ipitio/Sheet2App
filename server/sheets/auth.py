996ed99da9f6d23a797f220dd4aab292f4ed7e79
allen-siu <allenlsiu@gmail.com>
2023-04-05 23:30:55 -0400

    implemented oauth login api call for creating tokens, returns the users email

--- server/sheets/auth.py
+++ server/sheets/auth.py
@@ -1,48 +1,54 @@
 import os
 from http import HTTPStatus
-import requests
 
 from dotenv import get_key
 from dotenv import load_dotenv
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google_auth_oauthlib.flow import Flow
-from google.auth.transport.requests import Request
-
-from sheets.sheets_api import SCOPES
+from googleapiclient.discovery import build
 
 
 def get_client_id_secret():
     load_dotenv()
-    client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
-    
-    return client_id, client_secret
+    CLIENT_ID = os.getenv('CLIENT_ID')
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+    return CLIENT_ID, CLIENT_SECRET
 
 
 def oauth_login_user(auth_code):
     try:
         os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
         
-        client_id, client_secret = get_client_id_secret()
-        
-        # TODO: scopes need to be fixed so they match tszhim's scopes in google cloud oauth consent 
-        # fix this so it uses, Flow.from_client_config
-        flow = Flow.from_client_secrets_file(
-            'sheets/credentials_web.json',
-            scopes=['https://www.googleapis.com/auth/spreadsheets',
-                    "openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"],
+        CLIENT_ID, CLIENT_SECRET = get_client_id_secret()
+        CLIENT_CONFIG = {
+            "web": {
+                "client_id": CLIENT_ID,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "client_secret": CLIENT_SECRET
+            }
+        }
+        SCOPES = [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'openid',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ]
+
+        flow = Flow.from_client_config(
+            client_config=CLIENT_CONFIG,
+            scopes=SCOPES,
             redirect_uri="http://localhost:3000"
         )
         
         flow.fetch_token(code=auth_code)
+        credentials = flow.credentials
         
-        creds = flow.credentials
-        creds_info = Request().authorize(creds, verify=False)   # TODO this line is the next line that is bugged
+        service = service = build('oauth2', 'v2', credentials=credentials)
         
-        email = creds_info['email']
-        access_token = creds.token
-        refresh_token = creds.refresh_token
+        user_info = service.userinfo().get().execute()
+        email = user_info['email']
+        access_token = credentials.token
+        refresh_token = credentials.refresh_token
         
         return email, access_token, refresh_token, HTTPStatus.OK
     
