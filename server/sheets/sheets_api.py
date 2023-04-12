a57d5a6af8e4685d7e4a2c1d29214633e485fad8
ipitio <21136719+ipitio@users.noreply.github.com>
2023-04-12 01:43:36 -0400

    deserialize

--- server/sheets/sheets_api.py
+++ server/sheets/sheets_api.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import json
 
 import os.path
 
@@ -26,7 +27,7 @@ def get_creds():
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     elif os.getenv("TOKEN_JSON"):
-        creds = get_credentials(os.getenv("TOKEN_JSON"))
+        creds = get_credentials(json.loads(os.getenv("TOKEN_JSON", "")))
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
