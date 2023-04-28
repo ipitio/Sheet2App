71e7e1c479c947972081deff435e611d11670835
alexanderleong1 <alexander.leong@stonybrook.edu>
2023-04-28 10:36:27 -0400

    edit detail view cols

--- client/src/components/S2A/detailviews/EditAppDetailviewColumns.tsx
+++ client/src/components/S2A/detailviews/EditAppDetailviewColumns.tsx
@@ -16,278 +16,9 @@ import { editDetailviewColumns, viewDetailviewColumns } from '../../../store/Sto
 function EditAppDetailviewColumns() {
     const dispatch = useDispatch<typeof store.dispatch>();
 
-    useEffect(() => {
-        dispatch(viewDetailviewColumns());
-    }, []);
-
      /* Redux hooks into store. */
     const detailviewColumns = useSelector((state: StoreState) => state.S2AReducer.detailviewColumns);
     const editFilterColumn = useSelector((state: StoreState) => state.S2AReducer.editFilterColumn);
-
-    /*const detailviewColumns: Column[] = [
-        {
-          id: 1,
-          name: 'Column 1',
-          initialValue: 'Initial value 1',
-          isLabel: true,
-          isRef: false,
-          type: 'Text',
-          isFilter: true,
-          isUserFilter: false,
-          isEditFilter: true,
-          viewable: true,
-          editable: false
-        },
-        {
-          id: 2,
-          name: 'Column 2',
-          initialValue: 'Initial value 2',
-          isLabel: false,
-          isRef: true,
-          type: 'Number',
-          isFilter: false,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: null,
-          editable: null
-        },
-        {
-          id: 3,
-          name: 'Column 3',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'URL',
-          isFilter: true,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: false,
-          editable: true
-        },
-        {
-          id: 4,
-          name: 'Column 4',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'Text',
-          isFilter: true,
-          isUserFilter: false,
-          isEditFilter: false,
-          viewable: true,
-          editable: false
-        },
-        {
-          id: 5,
-          name: 'Column 5',
-          initialValue: '',
-          isLabel: true,
-          isRef: false,
-          type: 'Number',
-          isFilter: false,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: null,
-          editable: null
-        },
-        {
-          id: 6,
-          name: 'Column 6',
-          initialValue: '',
-          isLabel: false,
-          isRef: true,
-          type: 'Text',
-          isFilter: true,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: false,
-          editable: true
-        },
-        {
-          id: 7,
-          name: 'Column 7',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'URL',
-          isFilter: false,
-          isUserFilter: false,
-          isEditFilter: true,
-          viewable: true,
-          editable: true
-        },
-        {
-          id: 8,
-          name: 'Column 8',
-          initialValue: 'Initial value 8',
-          isLabel: true,
-          isRef: false,
-          type: 'Text',
-          isFilter: false,
-          isUserFilter: false,
-          isEditFilter: true,
-          viewable: false,
-          editable: false
-        },
-        {
-          id: 9,
-          name: 'Column 9',
-          initialValue: '',
-          isLabel: false,
-          isRef: true,
-          type: 'Number',
-          isFilter: true,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: null,
-          editable: null
-        },
-        {
-          id: 10,
-          name: 'Column 10',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'Text',
-          isFilter: true,
-          isUserFilter: false,
-          isEditFilter: false,
-          viewable: false,
-          editable: true
-        },
-        {
-          id: 11,
-          name: 'Column 11',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'Text',
-          isFilter: true,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: false,
-          editable: true
-        },
-        {
-          id: 12,
-          name: 'Column 12',
-          initialValue: '',
-          isLabel: false,
-          isRef: true,
-          type: 'Number',
-          isFilter: true,
-          isUserFilter: false,
-          isEditFilter: false,
-          viewable: null,
-          editable: null
-        },
-        {
-          id: 13,
-          name: 'Column 13',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'URL',
-          isFilter: false,
-          isUserFilter: false,
-          isEditFilter: true,
-          viewable: true,
-          editable: true
-        },
-        {
-          id: 14,
-          name: 'Column 14',
-          initialValue: '',
-          isLabel: true,
-          isRef: false,
-          type: 'Text',
-          isFilter: false,
-          isUserFilter: false,
-          isEditFilter: true,
-          viewable: false,
-          editable: false
-        },
-        {
-          id: 15,
-          name: 'Column 15',
-          initialValue: '',
-          isLabel: false,
-          isRef: true,
-          type: 'Number',
-          isFilter: true,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: null,
-          editable: null
-        },
-        {
-          id: 16,
-          name: 'Column 16',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'Text',
-          isFilter: true,
-          isUserFilter: false,
-          isEditFilter: false,
-          viewable: false,
-          editable: true
-        },
-        {
-          id: 17,
-          name: 'Column 17',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'URL',
-          isFilter: true,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: false,
-          editable: false
-        },
-        {
-          id: 18,
-          name: 'Column 18',
-          initialValue: '',
-          isLabel: false,
-          isRef: true,
-          type: 'Number',
-          isFilter: false,
-          isUserFilter: false,
-          isEditFilter: true,
-          viewable: true,
-          editable: true
-        },
-        {
-          id: 19,
-          name: 'Column 19',
-          initialValue: '',
-          isLabel: false,
-          isRef: false,
-          type: 'Text',
-          isFilter: false,
-          isUserFilter: true,
-          isEditFilter: false,
-          viewable: null,
-          editable: null
-        },
-        {
-          id: 20,
-          name: 'Column 20',
-          initialValue: '',
-          isLabel: true,
-          isRef: false,
-          type: 'URL',
-          isFilter: true,
-          isUserFilter: false,
-          isEditFilter: false,
-          viewable: false,
-          editable: true
-        },
-     ]*/
-
-    // const editFilterColumn = [true, true, true, false, false, true, false, true, false, true];
 
     /* React state for conditional rendering. */
     const [display, setDisplay] = useState<string>("Columns");
@@ -295,7 +26,19 @@ function EditAppDetailviewColumns() {
     /* React state for detailview columns. */
     const [changedColumns, setColumns] = useState<Column[]>(detailviewColumns);
     const [changedEditFilter, setEditFilter] = useState<boolean[] | null>(editFilterColumn);
-    const [shouldUseStore, setShouldUseStore] = useState(true);
+
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        if (!isLoading) return;
+
+        dispatch(viewDetailviewColumns())
+        .then(() => {
+          setColumns(detailviewColumns);
+          setEditFilter(editFilterColumn);
+
+          setIsLoading(false);
+        })
+    });
 
     /* Event handlers. */
 
@@ -319,14 +62,12 @@ function EditAppDetailviewColumns() {
 
     /* If the viewable checkbox is checked/unchecked. */
     const handleViewableChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setShouldUseStore(false);
-
         const viewableCheckbox = event.currentTarget as HTMLInputElement;
         const colToEditIdx = changedColumns.findIndex(col => col.id === Number(viewableCheckbox.id));
         const newViewable = viewableCheckbox.checked;
 
         if(colToEditIdx != -1) {
-            const newColumns = [...(shouldUseStore ? detailviewColumns : changedColumns)];
+            const newColumns = [...changedColumns];
             newColumns[colToEditIdx] = { ...newColumns[colToEditIdx], viewable: newViewable};
             setColumns(newColumns);
         }
@@ -334,24 +75,14 @@ function EditAppDetailviewColumns() {
 
     /* If the editable checkbox is checked/unchecked. */
     const handleEditableChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setShouldUseStore(false);
-
         const editableCheckbox = event.currentTarget as HTMLInputElement;
         const colToEditIdx = changedColumns.findIndex(col => col.id === Number(editableCheckbox.id));
         const newEditable = editableCheckbox.checked;
 
         if(colToEditIdx != -1) {
-          let newColumns;
-          if (detailviewColumns) {
-            newColumns = [...(shouldUseStore ? detailviewColumns : changedColumns)];
-          } else {
-            newColumns = [...changedColumns];
-          }
-
-          if (newColumns) {
+            const newColumns = [...changedColumns];
             newColumns[colToEditIdx] = { ...newColumns[colToEditIdx], editable: newEditable};
             setColumns(newColumns);
-          }
         }
     }
 
@@ -369,23 +100,13 @@ function EditAppDetailviewColumns() {
 
     /* If an edit filter checkbox is checked/unchecked. */
     const handleEditFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setShouldUseStore(false);
-
         const editFilterCheckbox = event.currentTarget as HTMLInputElement;
         const newEditFilterValue = editFilterCheckbox.checked;
 
         if(changedEditFilter) {
-          let newEditFilter;
-          if (editFilterColumn) {
-            newEditFilter = [ ...(shouldUseStore ? editFilterColumn : changedEditFilter) ];
-          } else {
-            newEditFilter = [ ...changedEditFilter ];
-          }
-
-          if (newEditFilter) {
+            const newEditFilter = [ ...changedEditFilter ];
             newEditFilter[Number(editFilterCheckbox.id)] = newEditFilterValue;
             setEditFilter(newEditFilter);
-          }
         }
     }
 
@@ -405,6 +126,7 @@ function EditAppDetailviewColumns() {
                 {/* Swap Display Button */}
                 <IconButton onClick={handleSwapDisplay} title="Swap">
                     <SwapHorizIcon fontSize="large"/>
+                    {`Edit ${display}`}
                 </IconButton>
                 
                 {/* Display Columns or Filters */}
