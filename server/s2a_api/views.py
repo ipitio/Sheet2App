bff5dbd813652bd9d2d109b38b9b9ff357de0284
allen-siu <allenlsiu@gmail.com>
2023-04-28 07:29:24 -0400

    implemented get accessible apps

--- server/s2a_api/views.py
+++ server/s2a_api/views.py
@@ -161,8 +161,38 @@ def get_developable_apps(request):
 
 @csrf_exempt
 def get_usable_apps(request):
-    # uses sheets api
-    return HttpResponse({}, status=HTTPStatus.OK)
+    body = json.loads(request.body)
+    tokens = parse_tokens(request)
+    creator_email = body["email"]
+    
+    apps, response_code = queries.get_all_published_apps_with_creator_email()
+    if response_code != HTTPStatus.OK:
+        return HttpResponse({}, status=response_code)
+    
+    accessible_apps = []
+    for app in apps:
+        role_mem_url = app["role_mem_url"]
+        
+        spreadsheet_id = sheets.utils.get_spreadsheet_id(role_mem_url)
+        gid = sheets.utils.get_gid(role_mem_url)
+        
+        roles_columns, response_code = sheets_api.get_data(
+            tokens=tokens, spreadsheet_id=spreadsheet_id, sheet_id=gid, majorDimension="COLUMNS"
+        )
+        if response_code != HTTPStatus.OK:
+            return HttpResponse({}, status=response_code)
+
+        for role_col in roles_columns:
+            if creator_email in role_col[1:]:
+                accessible_apps.append(app)
+    
+    
+    res_body = {"apps": accessible_apps }
+    response = HttpResponse(
+        json.dumps(res_body, cls=ExtendedEncoder), status=response_code
+    )
+
+    return response
 
 
 @csrf_exempt
