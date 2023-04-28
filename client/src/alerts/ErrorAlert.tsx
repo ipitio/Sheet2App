c081e807dcdaf304b64edebefa2727513e6fd15c
alexanderleong1 <alexander.leong@stonybrook.edu>
2023-04-28 13:09:41 -0400

    fixed positioning of error alert

--- client/src/alerts/ErrorAlert.tsx
+++ client/src/alerts/ErrorAlert.tsx
@@ -2,14 +2,14 @@ import Alert from '@mui/material/Alert';
 import { useDispatch, useSelector } from 'react-redux';
 import store, { StoreState, hideErrorAlert } from '../store/StoreContext';
 
-export default function SuccessAlert() {
+export default function ErrorAlert() {
     const dispatch = useDispatch<typeof store.dispatch>();
     const showSuccessAlert = useSelector((state: StoreState) => state.S2AReducer.showErrorAlert);
 
     return (
         <>
             {showSuccessAlert &&
-                <Alert severity="success" onClose={() => {dispatch(hideErrorAlert())}} sx={{marginTop: '75vh'}}>
+                <Alert severity="error" onClose={() => {dispatch(hideErrorAlert())}} sx={{marginBottom: '0vh'}}>
                     Error. Could not process your request.
                 </Alert>
             }
