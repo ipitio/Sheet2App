2191835de4b05675fd1e8105f98363a0ef592ae1
alexanderleong1 <alexander.leong@stonybrook.edu>
2023-05-01 19:37:09 -0400

    error message return to s2a button

--- client/src/alerts/ErrorAlert.tsx
+++ client/src/alerts/ErrorAlert.tsx
@@ -1,6 +1,9 @@
 import Alert from '@mui/material/Alert';
+import Link from '@mui/material/Link';
 import { useDispatch, useSelector } from 'react-redux';
-import { store, StoreState, hideErrorAlert, hideWebAppErrorAlert, clearErrorMessage } from '../store/StoreContext';
+import { store, StoreState, hideErrorAlert, hideWebAppErrorAlert, clearErrorMessage, returnToS2A } from '../store/StoreContext';
+import { Button } from '@mui/material';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 
 export default function ErrorAlert() {
     const dispatch = useDispatch<typeof store.dispatch>();
@@ -11,8 +14,16 @@ export default function ErrorAlert() {
     return (
         <>
             {(S2AShowErrorAlert || userAppShowErrorAlert) &&
-                <Alert severity="error" onClose={() => {dispatch(hideErrorAlert()); dispatch(hideWebAppErrorAlert()); dispatch(clearErrorMessage())}} sx={{marginBottom: '0vh'}}>
+                <Alert severity="error" onClose={() => {dispatch(hideErrorAlert()); dispatch(hideWebAppErrorAlert()); dispatch(clearErrorMessage())}} sx={{marginBottom: '0vh', display: 'flex', flexDirection: 'column'}}>
                     {`Error. Could not process your request. ${errorMessage}`}
+
+                    {errorMessage.includes("The datasource is not valid") &&
+                        <Link href='/S2A/home/access'>
+                            <Button startIcon={<KeyboardReturnIcon />} onClick={() => { dispatch(returnToS2A()); dispatch(hideWebAppErrorAlert()); dispatch(clearErrorMessage()); }}>
+                                Return To S2A
+                            </Button>
+                        </Link>
+                    }
                 </Alert>
             }
         </>
